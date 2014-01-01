5ac00ca9a321efc7ddf15f5c898f8bee7b0cceb8
Isaac Boehman <isaac@bitsighttech.com>
2014-01-01 13:25:23 -0500

    Initial commit of static config

---
+++ config.clj
@@ -0,0 +1,14 @@
+[:site-title "yacn.me"
+ :site-description "Yet Another Clever Name"
+ :site-url "http://yacn.me"
+ :author-name "yacn"
+ :author-email "admin@yacn.me"
+ :copyrightyear 2014
+ :in-dir "resources/"
+ :out-dir "html/"
+ :default-template "default.clj"
+ :encoding "UTF-8"
+ :blog-as-index false
+ :create-archives false
+ :atomic-build true]
+
