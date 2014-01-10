8cafa704f9a5be8402648357edfed0ef40faa346
Isaac Boehman <isaac@bitsighttech.com>
2014-01-10 18:28:14 -0500

    updates personal blog to remove is3500 posts

--- resources/site/blog.clj
+++ resources/site/blog.clj
@@ -6,10 +6,12 @@
    [:h2 "Blog"]
    (map #(let [f % url (static.core/post-url f)
                [metadata _] (static.io/read-doc f)
+               tags (.split (:tags metadata) " ")
                date (static.core/parse-date
                      "yyyy-MM-dd" "dd MMM yyyy"
                      (re-find #"\d*-\d*-\d*" (str f)))]
+    (if (not (some #{"is3500"} tags))
      [:div
       [:h4.blog-title [:a {:href url} (:title metadata)]]
-      [:h5.blog-date date]])
+      [:h5.blog-date date]]))
      (take 8 (reverse (static.io/list-files :posts))))]]]
