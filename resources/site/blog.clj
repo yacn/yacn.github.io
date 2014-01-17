1e737f8ef3932371c821ccb06dfee3b841370f5b
Isaac Boehman <isaac@bitsighttech.com>
2014-01-17 01:34:17 -0500

    fixes tag exclusion for main blog

--- resources/site/blog.clj
+++ resources/site/blog.clj
@@ -10,7 +10,7 @@
                date (static.core/parse-date
                      "yyyy-MM-dd" "dd MMM yyyy"
                      (re-find #"\d*-\d*-\d*" (str f)))]
-    (if (not (some #{"is3500blog"} tags))
+    (if (not (some #{"is3500blog" "assignment1"} tags))
      [:div
       [:h4.blog-title [:a {:href url} (:title metadata)]]
       [:h5.blog-date date]]))
