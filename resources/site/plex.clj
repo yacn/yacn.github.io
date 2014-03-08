e7a4ae93b2b57b4ae41b7565264baa95d7e70c3b
Isaac Boehman <isaac@bitsighttech.com>
2014-03-08 17:57:16 -0500

    adds architecture post to plex page

--- resources/site/plex.clj
+++ resources/site/plex.clj
@@ -9,4 +9,6 @@
      [:dd "Plex Media Server I share. If you want an invite, "
        [:a {:href "/about.html"} "contact me"]]
     [:dt [:a {:href "https://waffle.io/yacn/gunter-issues"} "Issue Tracker"]]
-     [:dd "Use this to file a support ticket for any issues you have with Plex / GUNTER"]]]]]
+     [:dd "Use this to file a support ticket for any issues you have with Plex / GUNTER"]
+    [:dt [:a {:href "/2014/01/01/plex-saas-overview/"} "Plex Server Architecture"]]
+     [:dd "Blog post giving a brief overview of the architecture I use for managing Plex etc."]] ]]]
