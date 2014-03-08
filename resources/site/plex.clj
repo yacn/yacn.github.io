f9e6fe0cd4a2f6ba0b9599df6f09192460f8671f
Isaac Boehman <isaac@bitsighttech.com>
2014-03-08 15:46:44 -0500

    adds basic plex page to site

---
+++ resources/site/plex.clj
@@ -0,0 +1,12 @@
+{:title "yacn.me - Plex"}
+
+[:div
+ [:div.row
+  [:div.col-md-12
+   [:h2 "Plex Links and Info"]
+   [:dl
+    [:dt [:a {:href "http://h.yacn.me:32400/web/"} "GUNTER"]]
+     [:dd "Plex Media Server I share. If you want an invite, "
+       [:a {:href "/about.html"} "contact me"]]
+    [:dt [:a {:href "https://waffle.io/yacn/gunter-issues"} "Issue Tracker"]]
+     [:dd "Use this to file a support ticket for any issues you have with Plex / GUNTER"]]]]]
